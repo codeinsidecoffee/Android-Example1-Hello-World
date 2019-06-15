dbb4779e1fefbf7ab2b42b1965e2a05c6f576f1a
Nimesh Patel <codeinsidecoffee@gmail.com>
2019-06-15 11:27:14 +0530

    Initial commit

---
+++ app/src/main/java/com/mrlonewolfer/example31/MainActivity.java
@@ -0,0 +1,18 @@
+package com.mrlonewolfer.example31;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+/*
+
+Create “Hello World” application. That will display “Hello World” in the middle of the
+Screen in the red color with white background.
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
